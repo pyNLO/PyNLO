180d51c601f11da99f2dec18c85d0721fe0df19d
Gabe Ycas <ycasg@colorado.edu>
2015-12-28 22:03:43 -0700

    Move readme file.

--- PyNLO_Root/pynlo/media/fibers/__init__.py
+++ PyNLO_Root/pynlo/media/fibers/__init__.py
@@ -5,6 +5,6 @@ Created on Wed Apr 02 14:10:46 2014
 @author: dim1
 """
 
-from pynlo.media.fibers import MongoFiberLoader
+from pynlo.media.fibers import JSONFiberLoader
 from pynlo.media.fibers import fiber
 from pynlo.media.fibers.calculators import DTabulationToBetas
