ef9b8141c4ab15b22cf12b25eaa94f9fe88e325e
danhickstein <danhickstein@gmail.com>
2016-01-21 17:19:29 -0700

    changed Se -> S

--- src/pynlo/media/crystals/XTAL_AgGaS.py
+++ src/pynlo/media/crystals/XTAL_AgGaS.py
@@ -2,7 +2,7 @@
 """
 Created on Fri May 29 11:23:17 2015
 
-Sellemeier coefficients and nonlinear parameter for AsGaSe_2
+Sellemeier coefficients and nonlinear parameter for AsGaS
 This file is part of pyNLO.
 
     pyNLO is free software: you can redistribute it and/or modify
