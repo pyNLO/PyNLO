332d0032e8498104932817d82080179fb049b999
danhickstein <danhickstein@gmail.com>
2016-02-29 12:37:59 -0700

    incorporated requirements into setup.py for readthedocs.org compatibility

--- setup.py
+++ setup.py
@@ -4,12 +4,34 @@
 # By popular demand...
 from setuptools import setup
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:
+    import numpy as np
+    install_requires=[
+          "jsonpickle>=0.9.2",
+          "jsonschema>=2.5.1",
+          "lxml>=3.4.4",
+          "mock>=1.0.1",
+          "nose>=1.3.7",
+          "nose-cov>=1.6",
+          "nose-fixes>=1.3",
+          "numpy>=1.9.2",
+          "pyFFTW>=0.9.0",
+          "scipy>=0.15.1",
+          "unittest2>=1.0.1"
+          ]
+else:
+    np = None
+    install_requires=[]
+
+
 setup(name='pyNLO',
       version='0.1',
       description='Python nonlinear optics',
       author='Gabe Ycas',
       author_email='ycasg@colorado.edu',
       url='https://github.com/ycasg/PyNLO',
+      install_requires=install_requires,
       packages=['pynlo',
                 'pynlo.devices',
                 'pynlo.interactions',
